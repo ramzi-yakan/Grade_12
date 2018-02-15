167b4452d3cc8107e0fe2308ba0ee451a7a7f066
graypaladin <36462615+graypaladin@users.noreply.github.com>
2018-02-15 16:45:11 -0500

    maze

--- Maze_Project/main.cpp
+++ Maze_Project/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void drawMaze(char maze[15][15]);
 void moveSprite(int y, int x, char maze[15][15]);
+void checkIfCollide();
 
 int main()
 {
@@ -18,12 +19,12 @@ int main()
                         (char)186, (char)255, (char)200, (char)205, (char)202, (char)205, (char)188, (char)255, (char)186, (char)255, (char)186, (char)255, (char)200, (char)205, (char)185,
                         (char)186, (char)255, (char)255, (char)255, (char)255, (char)255, (char)255, (char)255, (char)186, (char)255, (char)255, (char)255, (char)255, (char)255, (char)186,
                         (char)204, (char)205, (char)205, (char)205, (char)203, (char)205, (char)187, (char)255, (char)186, (char)255, (char)201, (char)205, (char)187, (char)255, (char)186,
-                        (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)186, (char)255, (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)186,
-                        (char)186, (char)255, (char)186, (char)255, (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)186, (char)255, (char)186, (char)255, (char)186,
+                        (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)186,
                         (char)186, (char)255, (char)186, (char)255, (char)200, (char)205, (char)188, (char)255, (char)186, (char)255, (char)200, (char)205, (char)188, (char)255, (char)186,
-                        (char)186, (char)255, (char)204, (char)205, (char)205, (char)255, (char)201, (char)205, (char)186, (char)205, (char)205, (char)205, (char)187, (char)255, (char)186,
-                        (char)186, (char)255, (char)200, (char)205, (char)205, (char)205, (char)185, (char)205, (char)202, (char)205, (char)205, (char)205, (char)188, (char)255, (char)186,
-                        (char)200, (char)205, (char)205, (char)205, (char)205, (char)205, (char)188, (char)255, (char)200, (char)205, (char)205, (char)205, (char)205, (char)205, (char)188};
+                        (char)186, (char)255, (char)186, (char)255, (char)255, (char)255, (char)255, (char)255, (char)186, (char)255, (char)255, (char)255, (char)255, (char)255, (char)186,
+                        (char)186, (char)255, (char)186, (char)255, (char)201, (char)205, (char)187, (char)255, (char)204, (char)205, (char)205, (char)205, (char)255, (char)255, (char)186,
+                        (char)186, (char)255, (char)186, (char)255, (char)255, (char)255, (char)186, (char)255, (char)186, (char)255, (char)255, (char)255, (char)255, (char)255, (char)186,
+                        (char)200, (char)205, (char)202, (char)205, (char)202, (char)205, (char)202, (char)205, (char)202, (char)205, (char)205, (char)176, (char)176, (char)205, (char)188};
     drawMaze(Map);
     int y_coord = 1;
     int x_coord = 3;
