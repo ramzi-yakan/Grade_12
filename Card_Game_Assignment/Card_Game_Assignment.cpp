193ad8ef953a0b72973cb58bab865151e6072ced
graypaladin <36462615+graypaladin@users.noreply.github.com>
2018-02-26 00:18:54 -0500

    cards

---
+++ Card_Game_Assignment/Card_Game_Assignment.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+struct card_t
+{
+    int value;
+    string suit;
+    int Rank;
+};
+
+struct pile
+{
+    int Size;
+};
+
+struct player
+{
+
+};
+
+void initializeDeck (card_t card)
+{
+
+}
+
+void displayCard
+
+int main()
+{
+    cout<<"h";
+}
