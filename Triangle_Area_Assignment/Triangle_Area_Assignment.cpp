6fd604e3a24a51522d780e3aadf0211327d76c1c
graypaladin <36462615+graypaladin@users.noreply.github.com>
2018-05-07 15:10:44 -0400

    triangle

--- Triangle_Area_Assignment/Triangle_Area_Assignment.cpp
+++ Triangle_Area_Assignment/Triangle_Area_Assignment.cpp
@@ -6,11 +6,10 @@ using namespace std;
 
 class Triangle
 {
-    float side1;
-    float side2;
-    float side3;
+    float side[3] = {0,0,0};
     float semiPerimeter;
     float area;
+    float height[3];
 public:
     void isTriangleValid()
     {
@@ -18,67 +17,114 @@ public:
         float smallerSide1;
         float smallerSide2;
         float smallerSideTotal;
-        while ((side1 <= 0) or (side2 <=0) or (side3 <= 0) or (largestSide >= smallerSideTotal))
+        while ((side[0] <= 0) or (side[1] <=0) or (side[2] <= 0) or (largestSide >= smallerSideTotal))
         {
             cout << "Please enter 3 side lengths." << endl;
             cout << "Side 1: ";
-            cin >> side1;
+            cin >> side[0];
             cout << endl;
             cout << "Side 2: ";
-            cin >> side2;
+            cin >> side[1];
             cout << endl;
             cout << "Side 3: ";
-            cin >> side3;
+            cin >> side[2];
             cout << endl;
-            if ((side1 > side2) and (side1 > side3))
+            if ((side[0] > side[1]) and (side[0] > side[2]))
             {
-                largestSide = side1;
-                smallerSide1 = side2;
-                smallerSide2 = side3;
+                largestSide = side[0];
+                smallerSide1 = side[1];
+                smallerSide2 = side[2];
             }
-            else if ((side2 > side1) and (side2 > side3))
+            else if ((side[1] > side[0]) and (side[1] > side[2]))
             {
-                largestSide = side2;
-                smallerSide1 = side1;
-                smallerSide2 = side3;
+                largestSide = side[1];
+                smallerSide1 = side[0];
+                smallerSide2 = side[2];
             }
-            else if ((side3 > side1) and (side3 > side2))
+            else if ((side[2] > side[0]) and (side[2] > side[1]))
             {
-                largestSide = side3;
-                smallerSide1 = side1;
-                smallerSide2 = side2;
+                largestSide = side[2];
+                smallerSide1 = side[0];
+                smallerSide2 = side[1];
             }
             smallerSideTotal = smallerSide1 + smallerSide2;
-            if ((side1 <= 0) or (side2 <=0) or (side3 <= 0) or (largestSide >= smallerSideTotal))
+            if ((side[0] <= 0) or (side[1] <=0) or (side[2] <= 0) or (largestSide >= smallerSideTotal))
             {
                 cout << "Not a valid triangle." << endl << endl;
             }
         }
     }
-    void getSemiPerimeter()
+    float getSemiPerimeter()
     {
-
+        semiPerimeter = (side[0] + side[1] + side[2])/2;
+        cout << "Semiperimeter: " << semiPerimeter << endl;
     }
-    void getArea()
+    float getArea()
     {
-
+        area = sqrt(semiPerimeter*(semiPerimeter - side[0])*(semiPerimeter - side[1])*(semiPerimeter - side[2]));
+        cout << "Area: " << area << endl;
     }
-    void getHeight()
+    float getHeight()
     {
-
+        for (int i = 0; i < 3; i++)
+        {
+            height[i] = 2*area/side[i];
+            cout << "Height from side "<< i+1 << ": " << height[i] << endl;
+        }
     }
     void getName()
     {
-
+        if ((side[0] == side[1]) and (side[0] == side[2]) and (side[1] == side[2]))
+        {
+            cout << "Type of Triangle: Equilateral" << endl;
+        }
+        else if ((side[0] != side[1]) and (side[0] != side[2]) and (side[1] != side[2]))
+        {
+            cout << "Type of Triangle: Scalene" << endl;
+        }
+        else
+        {
+            cout << "Type of Triangle: Isosceles" << endl;
+        }
     }
 };
+
+bool tryAgain()
+{
+    string answer;
+    while ((answer != "y") and (answer != "n"))
+    {
+        cout << "Try again?" << endl;
+        cin >> answer;
+        if (answer == "y")
+        {
+            return true;
+        }
+        else if (answer == "n")
+        {
+            return false;
+        }
+        else
+        {
+            cout << "Please provide a valid answer." << endl;
+        }
+    }
+}
 
 int main()
 {
     Triangle _triangle;
-    _triangle.isTriangleValid();
-    _triangle.getSemiPerimeter();
-    _triangle.getArea();
-    _triangle.getName();
+    bool programStatus = true;
+    while (programStatus == true)
+    {
+        _triangle.isTriangleValid();
+        _triangle.getSemiPerimeter();
+        _triangle.getArea();
+        _triangle.getHeight();
+        _triangle.getName();
+        cout << endl;
+        programStatus = tryAgain();
+        cout << endl << endl;
+    }
     return 0;
 }
